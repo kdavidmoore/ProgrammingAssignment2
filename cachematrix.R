0befb0ae149f0cb3990a4dd155e3fbdbd64bc10b
kdmoore89 <keith.m4@gmail.com>
2014-11-19 19:36:40 -0600

    Submission 1.2
    Making all the comments readable.

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,13 @@
 ## Used together, the following two functions return the inverse of a matrix.
-##Pass the matrix to be inverted as an argument to makeCacheMatrix and store the result in an object.
-##Then pass the object as an argument to cacheSolve.
-##Before calculating the inverted matrix, cacheSolve checks to see whether the inverted matrix has already been cached, thus saving precious hours of processing.
+## First, pass the matrix to be inverted as an argument to makeCacheMatrix and
+## store the result in an object. Then, pass the object as an argument to cacheSolve.
+## Before calculating the inverted matrix, cacheSolve checks to see whether the
+##  inverted matrix has already been cached, thus saving precious hours of processing.
 
 
-## makeCacheMatrix takes an invertible matrix as an argument and returns a list containing three functions: get, setinv, and getinv. These functions are later used by cacheSolve.
+## makeCacheMatrix takes an invertible matrix as an argument and returns a list
+## containing three functions: get, setinv, and getinv. These functions are
+## later used by cacheSolve.
 
 makeCacheMatrix <- function(x = matrix()) {
 	inv <- NULL
@@ -15,7 +18,10 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## cacheSolve takes the result of makeCacheMatrix as an argument and tests whether the inverted matrix has already been cached. If so, the function returns the inverted matrix from the cache. If not, the function calculates and returns the inverted matrix.
+## cacheSolve takes the result of makeCacheMatrix as an argument and tests whether
+## the inverted matrix has already been cached. If so, the function returns the
+## inverted matrix from the cache. If not, the function calculates and returns
+## the inverted matrix.
 
 cacheSolve <- function(x, ...) {
 	inv <- x$getinv()
